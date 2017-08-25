0c7fb2abebad7fb75d9aabd1e84ff0ca9a3a22f2
MrJoCrafter <cstudios35511@gmail.com>
2017-08-25 18:16:24 -0500

    added all the team-related arrays

--- main.js
+++ main.js
@@ -5,13 +5,20 @@ var defaultMaxTeamSize = 6;
 //Player names array
 var playernames = [];
 
-//Non-static variables related to 
+//Non-static variables related to  players
 var pCountFloor = 8;
-var pCountCeiling
+var pCountCeiling = 48;
 var AMNTofPlayers = defaultPlayers;
+
+//Arrays related to teams
+var teamNumberExists = [];
+var team1 = [];
+var team2 = [];
+var team3 = [];
+var team4 = [];
 
 //Non-static variables related to teams
 var teamsExist = false;
-var maxMaxTeamSize = int(AMNTofPlayers / 3);
+var maxMaxTeamSize = int(AMNTofPlayers / 4);
 var maxTeamSize = defaultMaxTeamSize;
 var maxTeamLimit = int(AMNTofPlayers / maxMaxTeamSize);
