16d82fb800ba131c17e0e4471d11114a621e6c9d
MrJoCrafter <cstudios35511@gmail.com>
2017-08-25 18:00:26 -0500

    Add a bazillion variables

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-var settings = ["1","2","3"];
+//Defaults
+var defaultPlayers = 24;
+var defaultMaxTeamSize = 6;
+
+//Player names array
+var playernames = [];
+
+//Non-static variables related to 
+var pCountFloor = 8;
+var pCountCeiling
+var AMNTofPlayers = defaultPlayers;
+
+//Non-static variables related to teams
+var teamsExist = false;
+var maxMaxTeamSize = int(AMNTofPlayers / 3);
+var maxTeamSize = defaultMaxTeamSize;
+var maxTeamLimit = int(AMNTofPlayers / maxMaxTeamSize);
