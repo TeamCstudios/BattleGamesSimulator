60d17dc24bb6813e02080f1e4e2f69b629922790
MrJoCrafter <cstudios35511@gmail.com>
2017-08-26 09:07:52 -0500

    Added new variables, reorganized, added new comments.

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
+//Constants
+var pCountFloor = 8;
+var pCountCeiling = 48;
+var gameSpeedFloor = 1;
+var gameSpeedCeiling = 5;
+
 //Defaults
 var defaultPlayers = 24;
 var defaultMaxTeamSize = 6;
+var defaultScatterSetting = 1;
+var defaultGameSpeed = 3;
 
 //Player names array
 var playerNames = [];
 
-//Other player-related arrays
+/* Other player-related arrays
+These store each player's statistics */
 var playersAlive = [];
+//Personality Status effects joining alliances.
 var playerPersonalityStatus = [];
+//Physical Status effects performance in fights. The higher it is, the more injured a player is.
 var playerPhysicalStatus = [];
+//Mental Status effects ability to heal, or stay in an alliance.
 var playerMentalStatus = [];
 
-//Non-static variables related to  players
-var pCountFloor = 8;
-var pCountCeiling = 48;
+//Non-static variables related to game settings
 var AMNTofPlayers = defaultPlayers;
+var gameSpeed = defaultGameSpeed;
+var teamsExist = false;
+var maxMaxTeamSize = int(AMNTofPlayers / 4);
+var maxTeamSize = defaultMaxTeamSize;
+var maxTeamLimit = int(AMNTofPlayers / maxMaxTeamSize);
 
 //Arrays related to teams
 var teamNumberExists = [];
@@ -22,12 +37,6 @@ var team1 = [];
 var team2 = [];
 var team3 = [];
 var team4 = [];
-
-//Non-static variables related to teams
-var teamsExist = false;
-var maxMaxTeamSize = int(AMNTofPlayers / 4);
-var maxTeamSize = defaultMaxTeamSize;
-var maxTeamLimit = int(AMNTofPlayers / maxMaxTeamSize);
 
 //This function initializes arrays, and will be run after the player count is changed
 function initializeArrays(){
@@ -43,17 +52,11 @@ function initializeArrays(){
 	
 }
 
-//Checks if a certain array contains something
-function arrayContains(array,value){
-	for(var i = 0; i < array.length; i++){
-		var cur = array[i];
-		if(cur = value){
-			return true;
-		}
-	}
-}
 
-//toggles whether teams exist or not.
+/* These functions handle game settings.
+These are usually tied to buttons. */
+
+//Toggles whether teams exist or not.
 function toggleTeams(){
 	if(teamsExist){
 		teamsExist = false;
@@ -72,4 +75,29 @@ function changePlayerCount(){
 		AMNTofPlayers = 8;
 	}
 	document.getElementById("playercountbutton").textContent = AMNTofPlayers + " Players";
+	initializeArrays();
+}
+
+//Changes the game speed
+function changeGameSpeed(){
+	if(AMNTofPlayers != 48) {
+		AMNTofPlayers = AMNTofPlayers + 8;
+	} else {
+		AMNTofPlayers = 8;
+	}
+	document.getElementById("playercountbutton").textContent = AMNTofPlayers + " Players";
+}
+
+
+/* These functions are functionality functions.
+They account for common processes. */
+
+//Checks if a certain array contains something
+function arrayContains(array,value){
+	for(var i = 0; i < array.length; i++){
+		var cur = array[i];
+		if(cur = value){
+			return true;
+		}
+	}
 }
